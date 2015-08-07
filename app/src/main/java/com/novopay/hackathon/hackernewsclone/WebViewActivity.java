33db489ce0e60c320863cfbb6ebb940881c22258
chinmaykrishna <seekay3@gmail.com>
2015-08-07 14:59:24 +0530

    javascript enabled and back button works on webview

--- app/src/main/java/com/novopay/hackathon/hackernewsclone/WebViewActivity.java
+++ app/src/main/java/com/novopay/hackathon/hackernewsclone/WebViewActivity.java
@@ -2,7 +2,10 @@ package com.novopay.hackathon.hackernewsclone;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by chinmaykrishna on 07/08/15.
@@ -15,6 +18,21 @@ public class WebViewActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview_activity);
         myWebView = (WebView) findViewById(R.id.webview);
+        //myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("http://www.bloomberg.com/news/features/2015-08-06/google-s-6-billion-miscalculation-on-the-eu?cmpid=BBD080615_BIZ");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 }
